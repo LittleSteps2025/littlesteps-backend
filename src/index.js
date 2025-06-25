4df48d058587c14761ac9b6e214c8fb5f50e19ac
R.K.Lakshan <lakshan2725@gmail.com>
2025-06-25 07:03:49 +0530

    backend file structure setup

---
+++ src/index.js
@@ -0,0 +1,34 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import pool from './config/db.js';
+import userRoutes from './routes/userRoutes.js'; // Importing user routes
+import errorHandler from './middlewares/errorHandler.js';
+import createUserTable from './data/createUserTable.js';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+//Middleware
+app.use(express.json());
+app.use(cors());
+
+//Routes
+app.use('/api', userRoutes); // Using user routes
+//Error handling middleware 
+app.use(errorHandler)
+//create table before starting the server
+createUserTable();                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   
+//Testing POSTGRES connection
+app.get("/", async (req, res) => {
+    console.log("Start");
+    const result =await pool.query('SELECT current_database()');
+    console.log("End");
+    res.send(`The database name is: ${result.rows[0].current_database}`);
+});
+//Server running
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
---
+++ src/routes/userRoutes.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import { createUser, deleteUser, getAllUsers, getUserById, updateUser } from '../controllers/userController.js';
+import validateUser from '../middlewares/inputValidator.js';
+
+const router = express.Router();
+
+router.post('/user',validateUser , createUser);
+router.get('/users', getAllUsers);
+router.get('/users/:id', getUserById);
+router.put('/users/:id',validateUser, updateUser);
+router.delete('/users/:id', deleteUser);
+
+
+export default router;
