524333422e168c6850c1b7f3ae83dd67a506103c
Rashmi Pathinayaka <140165202+rashmipathinayaka@users.noreply.github.com>
2025-10-19 21:22:35 +0530

    guardian image done

--- src/models/teacher/guardianModel.js
+++ src/models/teacher/guardianModel.js
@@ -4,20 +4,27 @@ const GuardianModel = {
   getGuardiansByChildId: async (childId) => {
     const result = await pool.query(
    `
-      SELECT u.name AS name
-      FROM child c
-      JOIN parent p ON c.parent_id = p.parent_id
-      JOIN "user" u ON p.user_id = u.user_id
-      WHERE c.child_id = $1
+     (
+  SELECT 
+    u.name AS name,
+    NULL AS image
+  FROM child c
+  JOIN parent p ON c.parent_id = p.parent_id
+  JOIN "user" u ON p.user_id = u.user_id
+  WHERE c.child_id = $1
+)
+UNION
+(
+  SELECT 
+    g.name AS name,
+    g.image AS image
+  FROM guardian g
+  WHERE g.parent_id = (
+      SELECT parent_id FROM child WHERE child_id = $1
+  )
+)
+ORDER BY name ASC;
 
-      UNION
-
-      SELECT g.name AS name
-      FROM guardian g
-      WHERE g.parent_id = (
-        SELECT parent_id FROM child WHERE child_id = $1
-      )
-      ORDER BY name ASC
       `, [childId]
     );
     return result.rows; // array of guardians with { name }
